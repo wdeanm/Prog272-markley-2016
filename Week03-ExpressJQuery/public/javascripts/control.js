a3880a07c632bcfc2fd75e3a289fb09270b36d00
Your Name <you@example.com>
2016-04-20 17:16:09 -0700

    ExpressJquery

---
+++ Week03-ExpressJQuery/public/javascripts/control.js
@@ -0,0 +1,43 @@
+/**
+ * Created by bcuser on 4/20/16.
+ */
+var MyObject = (function() {
+
+    // constructor
+    function MyObject() {
+        $('#sendString').click(showString);
+        $('#getItems').click(showItems);
+        $('#getMarie').click(showMarie);
+    }
+
+    function showString() {
+        $('#stringHolder').html('Send string was clicked');
+    }
+
+    function showItems() {
+        $("#myList").append('<li>' + 'item01' + '</li>');
+        $("#myList").append('<li>' + 'item02' + '</li>');
+        $("#myList").append('<li>' + 'item03' + '</li>');
+    }
+
+    function showMarie() {
+        var marie = {
+            "firstName": "Marie",
+            "lastName": "Curie",
+            "city": "Paris",
+            "country": "France"
+        };
+    }
+
+    MyObject.prototype.readyCalled = function() {
+        $("#readyCalled").html("Ready was called and myObjected created");
+    };
+
+    return MyObject;
+}());
+
+
+$(document).ready(function() {
+    var myObject = new MyObject();
+    myObject.readyCalled();
+});
