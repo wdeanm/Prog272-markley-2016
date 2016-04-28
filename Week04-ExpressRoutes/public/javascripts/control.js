e68f69b6214237a6414e34057ee12c16df666216
Your Name <you@example.com>
2016-04-27 17:18:43 -0700

    Week04-ExpressRoutes

--- Week04-ExpressRoutes/public/javascripts/control.js
+++ Week04-ExpressRoutes/public/javascripts/control.js
@@ -3,15 +3,34 @@
  */
 $(document).ready(function () {
     console.log("document loaded in prog 272");
-    $('#read').click(callRead);
-    function callRead() {
+
+    $('#read').click(read);
+    $('#readJson').click(readJson)
+    $('#readQux').click(readQux)
+
+
+    function read() {
         console.log('callRead called');
         $.getJSON('/read', function(result){
             console.log(result);
             $('#display').html(JSON.stringify(result));
-
         } )
 
     }
+    function readJson() {
+        console.log('callRead called');
+        $.getJSON('name.json', function(result){
+            console.log(result);
+            $('#display').html(JSON.stringify(result));
 
+        } )
+    }
+    function readQux() {
+        console.log('callQux called');
+        $.getJSON('/qux', function(result){
+            console.log(result);
+            $('#display').html(JSON.stringify(result));
+
+        } )
+    }
 })
--- Week04-ExpressRoutes/routes/index.js
+++ Week04-ExpressRoutes/routes/index.js
@@ -7,11 +7,29 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/read', function(request, response) {
+  console.log('Read was called on router')
   response.send([
     {"name": "SaraLee"},
     {"name": "Bob"}
   
   ]);
 });
+
+router.get('/qux', function(request, response) {
+  console.log('Qux was called on router')
+  response.send([
+    {"name": "LeeQux"},
+    {"name": "BobQux"}
+
+  ]);
+});
+
+router.get('/:id', function(request, response) {
+  response.send(request.params.id);
+});
+
+
+
+
 module.exports = router;
 
