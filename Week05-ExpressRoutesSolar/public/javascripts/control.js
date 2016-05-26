8acb429aef06806259acbdc8fc19954597b33257
Your Name <you@example.com>
2016-05-25 19:17:53 -0700

    l

--- Week05-ExpressRoutesSolar/public/javascripts/control.js
+++ Week05-ExpressRoutesSolar/public/javascripts/control.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
     'use strict';
-    $('#getRenewable').click(getRenewable)
+    $ ('#getRenewable').click(getRenewable)
+    $ ('#getRenewableByIndex').click(getRenewableByIndex)
+    $ ('#getRenewableByYear').click(getRenewableByYear)
 
     function getRenewable() {
         console.log('getRenewable called')
@@ -8,5 +10,41 @@ $(document).ready(function() {
             console.log(response);
             $('#debug').html(JSON.stringify(response, null, 4));
         })
+    }
+
+    function getRenewableByIndex() {
+        console.log('getRenewable called')
+        var userInput = $('#getRenewableByIndex').val();
+        $.getJSON('/renewablesByIndex' + userInput, function (response) {
+                console.log(response);
+                $('#debug').html(JSON.stringify(response,null, 4));
+            })
+            .done(function () {
+                console.log('second success')
+            })
+            .fail(function (a,b,c) {
+                console.log('Error, a,b,c')
+            })
+            .always(function () {
+                console.log('complete')
+            })
+    }
+
+    function getRenewableByYear() {
+        console.log('getRenewable called')
+        var userInput = $('#getRenewableByYear').val();
+        $.getJSON('/renewableByYear' + userInput, function (response) {
+                console.log(response);
+                $('#debug').html(JSON.stringify(response,null, 4));
+            })
+            .done(function () {
+                console.log('second success')
+            })
+            .fail(function (a,b,c) {
+                console.log('Error, a,b,c')
+            })
+            .always(function () {
+                console.log('complete')
+            })
     }
 });
